0e52a2a9a664334acf805fce8ead27c6bcc900ee
AshrafSadat <sayedashraffahim@gmail.com>
2022-01-04 21:32:53 -0500

    this is on SDETA branch

--- src/Git.java
+++ src/Git.java
@@ -4,9 +4,12 @@ public class Git {
 	public static void main(String[] args) {
 	
 	}
-	public void method A() {
-system.out.println ("this is the first change");
-	}
-public void method B() {
-system.out.println ("this is the second change");
+	public void methodA() {
+		System.out.println ("this is the first change");
+	} 
+	
+   public void methodB() {
+	System.out.println ("this is the second change");
+} 
+
 }
